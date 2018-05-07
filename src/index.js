db43ac19e2ac70e68a701fb805fde7dfe5514315
Mike Baranski <baranski@us.ibm.com>
2018-05-07 09:54:14 -0400

    Allow custom input component

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import './geo-coder.css';
+
 const STATE = {
-        ONE_FOUND: "one-found",
-        MULTIPLE_FOUND: "multiple-found",
-        NONE_FOUND: "none-found",
-        REQUEST_DENIED: "request-denied",
-        REQUEST_ERROR: "request-error",
-        SEARCHING: "searching",
-        READY: "ready"
+    ONE_FOUND: "one-found",
+    MULTIPLE_FOUND: "multiple-found",
+    NONE_FOUND: "none-found",
+    REQUEST_DENIED: "request-denied",
+    REQUEST_ERROR: "request-error",
+    SEARCHING: "searching",
+    READY: "ready"
+}
+
+class GeoCoderInput extends React.Component {
+    render() {
+        const inputId = 'geo-coder-' + new Date().valueOf()
+        return (
+            <div className={this.props.inputLabelWrapperClass}>
+                <label className={this.props.labelClass}
+                       htmlFor={inputId}><span>{this.props.fieldLabel}</span></label>
+                <input className={this.props.inputClass} id={inputId} disabled={this.props.searching}
+                       value={this.props.address}
+                       onChange={(event) => this.props.onChange(event.target.value)}/>
+            </div>
+        )
     }
+}
+
 class GeoCoder extends React.Component {
-    static DEFAULT_STATE(){
-return {
-        address: "",
-        foundAddresses: [],
-        api_status: null,
-        state: STATE.READY,
-        searching: false
-    }}
+    static DEFAULT_STATE() {
+        return {
+            address: "",
+            foundAddresses: [],
+            api_status: null,
+            state: STATE.READY,
+            searching: false
+        }
+    }
 
     constructor(props) {
         super(props)
         this.changeAddress = this.changeAddress.bind(this)
         this.searchAddress = this.searchAddress.bind(this)
         this.resetButton = this.resetButton.bind(this)
-
+        this.input = this.props.input
         this.state = GeoCoder.DEFAULT_STATE()
     }
 
@@ -78,6 +96,9 @@ return {
                         }
                     });
             }).catch((error) => {
+                component.setState({
+                    state: STATE.REQUEST_ERROR,
+                })
                 console.log("Error", error)
             }
         ).finally(() => {
@@ -87,35 +108,35 @@ return {
     }
 
     resetButton() {
-        return (<button className={this.props.resetButtonClass} disabled={this.state.searching} type="button" onClick={(event) => {
-            this.setState(GeoCoder.DEFAULT_STATE())
-        }}>
+        return (<button className={this.props.resetButtonClass} disabled={this.state.searching} type="button"
+                        onClick={(event) => {
+                            this.setState(GeoCoder.DEFAULT_STATE())
+                        }}>
             {this.props.resetButtonLabel}
         </button>)
     }
 
     render() {
         const component = this
-        const inputId = 'geo-coder-' + new Date().valueOf()
         return (
             <div style={{width: "350px"}}>
                 {this.props.showFormTitle ? <h2>{this.props.formTitle}</h2> : null}
                 <form className={this.props.formClass}>
                     <fieldset>
-                        {this.state.state === STATE.REQUEST_ERROR ? <p className={this.props.errorMessageClass}>{this.props.errorMessage}</p> : null}
-                        <div className={this.props.inputLabelWrapperClass}>
-                            <label className={this.props.labelClass} htmlFor={inputId}><span>{this.props.fieldLabel}</span></label>
-                            <input className={this.props.inputClass} id={inputId} disabled={this.state.searching} value={this.state.address}
-                                   onChange={(event) => this.changeAddress(event.target.value)}/>
-                        </div>
+                        {this.state.state === STATE.REQUEST_ERROR ?
+                            <p className={this.props.errorMessageClass}>{this.props.errorMessage}</p> : null}
+                        <this.input {...this.props} onChange={this.changeAddress} searching={this.state.searching} address={this.state.address}/>
                     </fieldset>
                     <div className={this.props.formButtonWrapperClass}>
-                        <button className={this.props.searchButtonClass} disabled={this.state.searching || this.state.state === STATE.ONE_FOUND} type="button" onClick={(event) => {
-                            component.searchAddress(component.state.address)
-                        }}>{this.props.searchButtonLabel}
+                        <button className={this.props.searchButtonClass}
+                                disabled={this.state.searching || this.state.state === STATE.ONE_FOUND} type="button"
+                                onClick={(event) => {
+                                    component.searchAddress(component.state.address)
+                                }}>{this.props.searchButtonLabel}
                         </button>
                         {this.resetButton()}
-                        <button className={this.props.acceptButtonClass} disabled={this.state.state !== STATE.ONE_FOUND} type="button"
+                        <button className={this.props.acceptButtonClass} disabled={this.state.state !== STATE.ONE_FOUND}
+                                type="button"
                                 onClick={(event) => this.props.onAddressAccept(component.state.foundAddresses[0])}
                         >{this.props.acceptButtonLabel}
                         </button>
@@ -143,12 +164,13 @@ return {
                                         {val.formatted_address}
                                     </td>
                                     <td>
-                                        <button className={this.props.selectButtonClass} type="button" onClick={(event) => component.setState({
-                                            address: val.formatted_address,
-                                            foundAddresses: [val],
-                                            finalItem: val,
-                                            state: STATE.ONE_FOUND
-                                        })}>{this.props.selectButtonLabel}
+                                        <button className={this.props.selectButtonClass} type="button"
+                                                onClick={(event) => component.setState({
+                                                    address: val.formatted_address,
+                                                    foundAddresses: [val],
+                                                    finalItem: val,
+                                                    state: STATE.ONE_FOUND
+                                                })}>{this.props.selectButtonLabel}
                                         </button>
                                     </td>
                                 </tr>
@@ -193,6 +215,8 @@ GeoCoder.propTypes = {
     inputClass: PropTypes.string,
     labelClass: PropTypes.string,
     onAddressAccept: PropTypes.func,
+
+    input: PropTypes.element,
 }
 
 GeoCoder.defaultProps = {
@@ -220,6 +244,7 @@ GeoCoder.defaultProps = {
     inputClass: "geo-code-input",
     labelClass: "geo-code-label",
 
+    input: GeoCoderInput,
 }
 
 export default GeoCoder
